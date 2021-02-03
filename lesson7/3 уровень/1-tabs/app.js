b16e5d25c0cad1360c0e4d1a61a3d16f8a3651c9
audetv <audetv@gmail.com>
2021-02-03 17:04:55 +0300

    Added solutions for level 3

--- lesson7/3 уровень/1-tabs/app.js
+++ lesson7/3 уровень/1-tabs/app.js
@@ -11,7 +11,12 @@ const texts = {
     2.1 Переберите полученную коллекцию, например с помощью forEach, и каждой ссылке назначьте
     обработчик клика функцию clickHandler.
 */
+let text = document.querySelector('.text');
+let navLinks = document.querySelectorAll('.nav-link');
 
+navLinks.forEach(function (navLink) {
+   navLink.addEventListener('click', clickHandler);
+});
 
 /**
  * Обработчик клика по .nav-link
@@ -20,7 +25,8 @@ const texts = {
 function clickHandler(event) {
     // здесь вызывайте changeText и changeActiveClass, и передавайте
     // им объект события.
-   
+   changeActiveClass(event);
+   changeText(event);
 }
 
 /**
@@ -29,7 +35,10 @@ function clickHandler(event) {
  * @param {MouseEvent} event 
  */
 function changeActiveClass(event) {
-    
+    navLinks.forEach(function (navLink) {
+       navLink.classList.remove('active');
+    });
+    event.currentTarget.classList.add('active');
 }
 
 /**
@@ -39,5 +48,15 @@ function changeActiveClass(event) {
  * @param {MouseEvent} event 
  */
 function changeText(event) {
-    
+    switch (event.target.textContent) {
+        case 'Link 1':
+            text.textContent = texts.text1;
+            break;
+        case 'Link 2':
+            text.textContent = texts.text2;
+            break;
+        case 'Link 3':
+            text.textContent = texts.text3;
+            break;
+    }
 }
